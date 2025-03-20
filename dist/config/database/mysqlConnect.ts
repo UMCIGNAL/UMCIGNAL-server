129c3096edeb304f0383c4aea6504d844f095943
Gjuni <kmj06178@gmail.com>
2025-03-20 14:10:44 +0900

    DB shecma file fixed

--- dist/config/database/mysqlConnect.ts
+++ dist/config/database/mysqlConnect.ts
@@ -3,13 +3,12 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-// eslint-disable-next-line import/prefer-default-export
 export const pool = mysql.createPool({
-  host: process.env.DATABASE_HOST, // SSH 터널을 통해 로컬로 리디렉션되므로 localhost 사용
-  user: process.env.DATABASE_USERNAME, // 데이터베이스 사용자 이름
-  password: process.env.DATABASE_PASSWORD, // 데이터베이스 비밀번호
-  database: process.env.DATABASE_NAME, // 데이터베이스 이름
-  port: Number(process.env.DATABASE_PORT), // 로컬 포워딩 포트
+  host: process.env.DATABASE_HOST, 
+  user: process.env.DATABASE_USERNAME, 
+  password: process.env.DATABASE_PASSWORD, 
+  database: process.env.DATABASE_NAME, 
+  port: Number(process.env.DATABASE_PORT), 
 });
 
 export const getPool = () => pool;
