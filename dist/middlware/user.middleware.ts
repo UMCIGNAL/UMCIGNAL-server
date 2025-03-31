bea42d53be0e17663a69e718a4694b1ccf6ec8e4
Gjuni <kmj06178@gmail.com>
2025-03-31 16:07:54 +0900

    check signUP and idleType function added

--- dist/middlware/user.middleware.ts
+++ dist/middlware/user.middleware.ts
@@ -1,3 +1,4 @@
+import { getPool } from "../config/database/mysqlConnect";
 
 export const convertAge = async (
     date_of_birth : Date
@@ -15,4 +16,44 @@ export const convertAge = async (
     }
 
     return age;
-}
+};
+
+
+export const checkSignUpInsert = async (
+    user_id : number
+): Promise<boolean> => {
+    const pool = await getPool();
+
+    const query = `SELECT signUpComplete FROM user WHERE user_id = ?;`;
+
+    const [result] : any = await pool.query(query, [user_id]);
+
+    const checking = result[0].signUpComplete;
+
+
+    // 이미 초기 유저 정보를 전부 입력한 거임
+    if(checking === 1) {
+        return true;
+    }
+
+    return false;
+};
+
+export const checkIdleTypeInsert = async (
+    user_id : number
+): Promise<boolean> => {
+    const pool = await getPool();
+
+    const query = `SELECT idleComplete FROM user WHERE user_id =?;`;
+
+    const [result] : any = await pool.query(query, [user_id]);
+
+    const checking = result[0].idleComplete;
+
+    // 이미 초기 유저 정보를 전부 입력한 거임
+    if(checking === 1) {
+        return true;
+    }
+
+    return false;
+};
