653e2fa0b5eaf33650813d6fcdf84ec26f721dee
Gjuni <kmj06178@gmail.com>
2025-04-03 19:44:30 +0900

    serial Code added func added

--- dist/serialCode/serialCode.router.ts
+++ dist/serialCode/serialCode.router.ts
@@ -1,9 +1,11 @@
 import { Router } from 'express';
 import { getCode } from '../middlware/serialCode.middleware';
+import { insertCodeController } from './serialCode.controller/serialCode.controller';
 
 const router = Router();
 
 // router.get('/getCode', getCode);
+router.patch('/insertCode', insertCodeController);
 
 /**
  * @ swagger
@@ -31,4 +33,69 @@ const router = Router();
  *         description: Internal server error
  */
 
+
+/**
+ * @swagger
+ * /serialCode/insertCode:
+ *   patch:
+ *     summary: "시리얼 코드 입력"
+ *     description: "사용자가 시리얼 코드를 입력하면 해당 코드가 유효한지 확인하고 추가합니다."
+ *     tags:
+ *       - Serial Code
+ *     security:
+ *       - BearerAuth: []
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               serialCode:
+ *                 type: string
+ *                 example: "ABCD1234"
+ *     responses:
+ *       202:
+ *         description: "코드가 정상적으로 추가됨"
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 result:
+ *                   type: integer
+ *                 message:
+ *                   type: string
+ *                   example: "코드가 정상적으로 추가되었습니다."
+ *       401:
+ *         description: "토큰이 유효하지 않거나 로그인되지 않음"
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   example: "로그인 되어있지 않습니다."
+ *       404:
+ *         description: "토큰 또는 코드가 입력되지 않음, 존재하지 않는 코드"
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   example: "존재하지 않는 코드입니다."
+ *       500:
+ *         description: "서버 에러"
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   example: "서버 에러입니다."
+ */
 export default router;
