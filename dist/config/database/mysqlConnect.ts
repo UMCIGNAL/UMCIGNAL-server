1a4dfb7d6e823fae26f4da9c21735eaeb351104c
Jun <112748587+Gjuni@users.noreply.github.com>
2025-05-09 10:30:22 +0900

    Dev mysqlConnect.ts

--- dist/config/database/mysqlConnect.ts
+++ dist/config/database/mysqlConnect.ts
@@ -1,13 +1,8 @@
 import mysql from 'mysql2/promise';
-import { Client } from 'ssh2';
 import dotenv from 'dotenv';
-import fs from 'fs';
-import path from 'path';
 
 dotenv.config();
 
-const sshClient = new Client();
-let pool: mysql.Pool;
 export const pool = mysql.createPool({
   host: process.env.DATABASE_HOST, 
   user: process.env.DATABASE_USERNAME, 
